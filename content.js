09e9d5117de5a0b26fda9759c37cc47ed9557e01
Valkyrie <Valkyrie121@users.noreply.github.com>
2019-06-14 21:17:42 -0400

    Added Redirect notice on facepunch.com

--- content.js
+++ content.js
@@ -1,2 +1,12 @@
 if (window.location.href.match('//forum.facepunch.com/'))
+{
     window.location.replace(`https://web.archive.org/web/${window.location.href}`)
+
+    document.getElementsByTagName('body')[0].innerHTML = `
+        <center style="position: relative; top: 200px; font-family: sans-serif; color: #444;">
+        <br/><br/>
+        <p>
+            Redirecting to <strong>archive.org</strong>
+        </p>
+    `; 
+}
