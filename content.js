a84a694fb6bcc9aa73934db75b5979eeccbc70c4
Valkyrie <Valkyrie121@users.noreply.github.com>
2019-06-14 20:43:23 -0400

    Add files via upload

---
+++ content.js
@@ -0,0 +1,2 @@
+if (window.location.href.match('//forum.facepunch.com/'))
+    window.location.replace(`https://web.archive.org/web/${window.location.href}`)
---
+++ redirect.js
@@ -0,0 +1,5 @@
+chrome.extension.onRequest.addListener(function(request, sender) 
+    {
+        chrome.tabs.update(sender.tab.id, {url: request.redirect})
+    }
+)
