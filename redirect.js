54c1014cc61e0219f388724702350b9df32c88c4
Valkyrie <Valkyrie121@users.noreply.github.com>
2019-06-30 12:07:08 -0400

    General Fixes
    Added support for /f/ "newpunch" links.

--- redirect.js
+++ redirect.js
@@ -1,3 +1,8 @@
+/*
+	Developed by Valkyrie
+	Discord: Valkyrie#666
+*/
+
 chrome.extension.onRequest.addListener(function(request, sender) 
     {
         chrome.tabs.update(sender.tab.id, {url: request.redirect})
