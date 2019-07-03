15f570ab449f875f16a3b84a3734e97276aeaaee
Valkyrie <Valkyrie121@users.noreply.github.com>
2019-07-02 20:12:26 -0400

    Add files via upload

---
+++ fpArchive.js
@@ -0,0 +1,17 @@
+if (window.location.href.match('//forum.facepunch.com/'))
+{
+	var newURL = window.location.href
+	if (newURL.match(/\/f\//g))
+		newURL = newURL.replace(/\/f\//g, "/")
+
+	window.location.replace(`https://web.archive.org/web/${newURL}`)
+
+	document.title = "FP Archive";
+	document.getElementsByTagName('body')[0].innerHTML = `
+		 <center style="position: relative; top: 200px; font-family: sans-serif; color: #444;">
+		<br/><br/>
+		<p>
+			Redirecting to <strong>archive.org</strong>
+		</p>
+	`; 
+}
